0146a67007aea1e4c61aef2fcee3b6e23804533a
Mark Mulder <markmulder@gmail.com>
2015-10-29 16:49:43 +0100

    Disable ES logging

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -6,9 +6,4 @@ es_port = ENV['NOTIFILTER_ESPORT'] || "9200"
 
 host_port = "#{es_host}:#{es_port}"
 
-if Rails.env.development?
-  log = true
-else
-  log = false
-end
-$ES = Elasticsearch::Client.new(host: host_port, log: log)
+$ES = Elasticsearch::Client.new(host: host_port)
